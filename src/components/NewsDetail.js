addf28e6c9245a58d82d1eba8e916576408cec58
Ovais Butt <ovaisbutt786@gmail.com>
2019-02-21 17:54:41 -0800

    Adding Styles in Comments

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -13,14 +13,18 @@ import {
 import HTMLView from "react-native-htmlview";
 import Icon from "react-native-vector-icons/Ionicons";
 import { Header, Left, Right } from "native-base";
-import axios from 'axios'
+import axios from "axios";
 
 const ACCESS_ID = "access_id";
 export default class NewsDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: this.props.navigation.state.params.data, id: '', commentPost: '' };
-    this.getToken()
+    this.state = {
+      data: this.props.navigation.state.params.data,
+      id: "",
+      commentPost: ""
+    };
+    this.getToken();
   }
   componentWillUnmount() {
     console.log("Component Will Unmount Called");
@@ -42,28 +46,41 @@ export default class NewsDetail extends Component {
       console.log("Cannot get token");
     }
   }
-  onPostButtonPressed(){
-    const{commentPost, id, data} = this.state;
+  onPostButtonPressed() {
+    const { commentPost, id, data } = this.state;
     let postId = data._id;
 
-    console.log('onPostButtonCalled')
-    console.log('Data of comments:', 'comment',commentPost, 'userID', id, 'PostID', postId)
+    console.log("onPostButtonCalled");
+    console.log(
+      "Data of comments:",
+      "comment",
+      commentPost,
+      "userID",
+      id,
+      "PostID",
+      postId
+    );
 
-    axios.post('http://198.245.53.50:5000/api/posts/add-comment',{
-      userId : id,
-      postId : postId,
-      comment : commentPost
-    })
-    .then(response => {console.log('Response Comment: ', response)})
-    .catch(error => {console.log('Error Comment: ', error)})
+    axios
+      .post("http://198.245.53.50:5000/api/posts/add-comment", {
+        userId: id,
+        postId: postId,
+        comment: commentPost
+      })
+      .then(response => {
+        console.log("Response Comment: ", response);
+      })
+      .catch(error => {
+        console.log("Error Comment: ", error);
+      });
   }
   renderItem = ({ item }) => {
     //item = item.filter(item=>item.breaking === false)
     return (
       <TouchableOpacity
-        // onPress={() => {
-        //   this.props.navigation.navigate("NewsDetail", { data: item });
-        // }}
+      // onPress={() => {
+      //   this.props.navigation.navigate("NewsDetail", { data: item });
+      // }}
       >
         {/* <View style={{ flexDirection: "row", width: 380 }}>
           <Image
@@ -71,16 +88,14 @@ export default class NewsDetail extends Component {
             source={{ uri: item.photoUrl }}
             style={styles.imageThumbStyle}
           /> */}
-          <View style={{ flexDirection: "column" }}>
-            {/* <Text style={styles.catagoryStyle}>{item.comment}</Text> */}
+        <View style={{ flexDirection: "column" }}>
+          {/* <Text style={styles.catagoryStyle}>{item.comment}</Text> */}
 
-            <Text style={styles.titleTextStyle}>
-              {item.comment}
-            </Text>
-            {/* <Text style={{ color: "white" }}>
+          <Text style={styles.titleTextStyle}>{item.comment}</Text>
+          {/* <Text style={{ color: "white" }}>
               {item.createdAt.substring(0, 10)}
             </Text> */}
-          </View>
+        </View>
         {/* </View> */}
       </TouchableOpacity>
     );
@@ -101,7 +116,7 @@ export default class NewsDetail extends Component {
     const { data } = this.state;
     let commentsGot = data.commentsGot;
     return (
-      <View style={styles.container}>
+      <View>
         <ScrollView>
           <Header style={{ backgroundColor: "white" }}>
             <Left>
@@ -117,15 +132,19 @@ export default class NewsDetail extends Component {
                 </TouchableOpacity>
 
                 <Text
-                  style={{ color: "black", fontSize: 18, fontWeight: "bold", marginLeft: 30, marginTop: 2 }}
+                  style={{
+                    color: "black",
+                    fontSize: 18,
+                    fontWeight: "bold",
+                    marginLeft: 30,
+                    marginTop: 2
+                  }}
                 >
                   Go Live
                 </Text>
               </View>
             </Left>
-            <Right>
-              
-            </Right>
+            <Right />
           </Header>
           <Text
             style={{
@@ -153,7 +172,7 @@ export default class NewsDetail extends Component {
             <Text style={{ color: "black", marginLeft: 10 }}>Tags: </Text>
             <Text>{data.tags}</Text>
           </View>
-          
+
           <Image
             source={{ uri: data.photoUrl }}
             style={{
@@ -163,7 +182,7 @@ export default class NewsDetail extends Component {
               marginTop: 8
             }}
           />
-          
+
           <View
             style={{
               marginTop: 5,
@@ -184,29 +203,44 @@ export default class NewsDetail extends Component {
               flex: 1
             }}
           >
-          
             <HTMLView value={data.description} stylesheet={styles} />
           </View>
 
-          <FlatList
-          data={data.commentsGot}
-          keyExtractor={item => item._id}
-          renderItem={this.renderItem}
-          ItemSeparatorComponent={this.renderSeparator}
+          <Text style={{color: 'black', fontSize: 25, fontWeight: 'bold', marginLeft: 20}}>Comments</Text>
 
-          ></FlatList>
-          
+          <View  style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 10,
+                paddingLeft: "5%",
+                paddingRight: "5%",
+                marginBottom: 80
+              }}>
+            <FlatList
+              data={data.commentsGot}
+              keyExtractor={item => item._id}
+              renderItem={this.renderItem}
+              ItemSeparatorComponent={this.renderSeparator}
+            />
+          </View>
         </ScrollView>
-        
-        <View style={{ width: '100%' , flexDirection: "row", position: "absolute", bottom: 0, backgroundColor: 'white' }}>
+
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            position: "absolute",
+            bottom: 0,
+            backgroundColor: "white"
+          }}
+        >
           <TextInput
             placeholder="Type a comment..."
             scrollEnabled={true}
             multiline={true}
             onChangeText={commentPost => this.setState({ commentPost })}
             value={this.state.commentPost}
-
-
             style={{
               fontSize: 16,
               borderRadius: 25,
@@ -218,16 +252,19 @@ export default class NewsDetail extends Component {
               paddingLeft: 15
             }}
           />
-          <TouchableOpacity onPress={()=>{this.onPostButtonPressed()}}>
-          <Icon
-            name="ios-send"
-            size={40}
-            color="#003366"
-            style={{ marginTop: 15 }}
-          />
+          <TouchableOpacity
+            onPress={() => {
+              this.onPostButtonPressed();
+            }}
+          >
+            <Icon
+              name="ios-send"
+              size={40}
+              color="#003366"
+              style={{ marginTop: 15 }}
+            />
           </TouchableOpacity>
         </View>
-        
       </View>
     );
   }
@@ -257,5 +294,5 @@ const styles = StyleSheet.create({
     color: "black",
     paddingTop: 10,
     paddingBottom: 10
-  },
+  }
 });
